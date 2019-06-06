6b8e13d4da01fdf9dd32a2268de9d30f220f0cfc
ckaoki <cullan_aoki@msn.com>
2019-06-05 17:50:10 -0700

    implemented radio buttons for answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+//TODO: variables: correct, incorrect, unanswered
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+var Trivia = [
+    {question:"q0", answer:"a0"},
+    {question:"q1", answer:"a1"},
+    {question:"q2", answer:"a2"},
+    {question:"q3", answer:"a3"},
+    {question:"q4", answer:"a4"},
+    {question:"q5", answer:"a5"},
+    {question:"q6", answer:"a6"},
+    {question:"q7", answer:"a7"},
+    {question:"q8", answer:"a8"},
+    {question:"q9", answer:"a9"},
+]
+
+function init(){
+
+}
+
+//TODO: display start button to start game. init ();
+//TODO: when start button pressed reset variables.
+//TODO: load first question. question has mutually exclusive answers
+//TODO: start timer. Possibly display spinner.
+//TODO: if answered within time show whether correct of not.
+//TODO: if answer correct display correct answer, "Correct!", and increment correct variable
+//TODO: if answer incorrect display correct answer, "Incorrect" and increment incorrect variable.
+//TODO: if no answer within time show correct answer and "Out of time!"
+//TODO: after last question display correct, incorrect, and unanswered. with "Start Over?" button
+//TODO: "Start over?" button restarts the game. init();
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    //  $("#answer4").attr("class", "form-check visible");
+    //  $("#temp").text(Trivia[1].question);
+});
